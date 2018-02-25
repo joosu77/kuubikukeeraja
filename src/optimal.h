80823fa115431ffe97b8508181f9d187afddb671
Joosep Naks <joosep.naks@mail.ee>
2018-02-25 19:47:39 +0200

    sisse toodud neti optimal solver

---
+++ src/optimal.h
@@ -0,0 +1,21 @@
+/*
+ * optimal.h
+ *
+ *  Created on: Feb 25, 2018
+ *      Author: joosep
+ */
+
+#ifndef OPTIMAL_H_
+#define OPTIMAL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int  solve_optimal (void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OPTIMAL_H_ */
