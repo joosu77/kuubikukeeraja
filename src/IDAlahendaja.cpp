6166b5529b333a42fc0ddbbf2b20d1615bf9b557
Joosep <joosep.naks@mail.ee>
2018-03-06 23:12:39 +0200

    uuendatud lihtsamini arusaadav idalahendaja

--- src/IDAlahendaja.cpp
+++ src/IDAlahendaja.cpp
@@ -17,56 +17,60 @@
 
 valem IDAlahendaja::lahenda(asend sisAsend) {
 	int attempts { 0 };
-	kuubik sisKuup {sisAsend};
 	std::unordered_set<asend> kaidud {};
 	std::vector<valem> pooleli { };
 
-	bool first { true };
 	valem lahend {};
-	bool korras { false };
 
-	while (!korras){
-		valem tee { };
-		asend olek { };
-		if (first){
-			olek = sisKuup.kuup;
-			first = false;
-		} else {
-			tee = pooleli[0];
-			std::cout << "alles: " << pooleli.size() << '\n';
-			pooleli.erase(pooleli.begin());
-
-			sisKuup.turn(tee);
-			olek = sisKuup.kuup;
-		}
-
-
-		if (sisKuup.isSolved()){
-			korras = true;
-			lahend.rida = tee.rida;
-		}
-		sisKuup.rewind();
-
-		if (kaidud.count(olek) == 0 && !korras){
-			for (int i=0;i<12;i++){
-				if(i<6){
-					valem uus = tee;
-					uus.append("ULFRDB"[i],true);
-					pooleli.push_back(uus);
-				} else {
-					valem uus =tee;
-					uus.append("ULFRDB"[i-6], false);
-					pooleli.push_back(uus);
-				}
-			}
-
-			kaidud.insert(olek);
-		}
-
+	kuubik *sisKuup = new kuubik {sisAsend};
+	bool korras { sisKuup->isSolved() };
+	while (!korras)  {
 		attempts++;
+
+		if (pooleli.size() > 0) {
+			// võtame ettevalmistatud valemite hulgast esimese
+			// ja proovime sellega lahendada
+			lahend = pooleli[0];
+			pooleli.erase(pooleli.begin());
+			sisKuup->turn(lahend);
+			korras = sisKuup->isSolved();
+		}
+
+		if (!korras) {
+			// ei lahenenud, lisame praeguse oleku alamolekud hulka juurde
+			if (kaidud.count(sisKuup->kuup) == 0){
+				// seda olekut pole veel külastatud
+				for (int i=0;i<12;i++){
+					if(i<6){
+						valem uus = lahend;
+						uus.append("ULFRDB"[i],true);
+						pooleli.push_back(uus);
+					} else {
+						valem uus = lahend;
+						uus.append("ULFRDB"[i-6], false);
+						pooleli.push_back(uus);
+					}
+				}
+
+				kaidud.insert(sisKuup->kuup);
+			} else if (pooleli.size() == 0) {
+				// jõudsime külastatud olekuni, ühtegi töötlemata valemit enam ei ole
+				// järelikult lahendit ei leitud
+				break;
+			}
+		}
+
+		// kerime kuubiku algolekusse tagasi
+		delete sisKuup;
+		sisKuup = new kuubik {sisAsend};
 	}
 
-	std::cout << "Kokku katseid: " << attempts;
-	return lahend;
+	std::cout << "Kokku katseid: " << attempts << std::endl;
+	if (korras) {
+		return lahend;
+	} else {
+		std::cout << "Lahendit ei leitud!" << std::endl;
+		return *(new valem {});
+	}
 }
 
